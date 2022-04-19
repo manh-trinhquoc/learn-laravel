ee86529f422a31e0e414edf09a01cb9064b177fc
Manhtq <trinhquocmanh1990@gmail.com>
2022-04-19 16:13:53 +0700

    sp

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/', 'WelcomeController@index');
+
+Route::resource('tasks', 'App\Http\Controllers\TaskController');
