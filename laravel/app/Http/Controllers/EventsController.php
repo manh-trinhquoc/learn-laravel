9ba2898a08e744a765e571023b7e4ea13d243d10
Manhtq <trinhquocmanh1990@gmail.com>
2022-04-21 17:40:27 +0700

    126

--- laravel/app/Http/Controllers/EventsController.php
+++ laravel/app/Http/Controllers/EventsController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Event;
+use Illuminate\Http\Request;
 
 class EventsController extends Controller
 {
@@ -14,8 +14,6 @@ class EventsController extends Controller
      */
     public function index()
     {
-        // $events = Event::all();
-        // $events = Event::paginate(10);
         $events = Event::simplePaginate(10);
 
         return view('events.index')->with('events', $events);
@@ -45,23 +43,21 @@ class EventsController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Models\Event  $event
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Event $event)
     {
-        $event = Event::find($id);
-
         return view('events.show')->with('event', $event);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Models\Event  $event
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Event $event)
     {
         //
     }
@@ -70,10 +66,10 @@ class EventsController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Models\Event  $event
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Event $event)
     {
         //
     }
@@ -81,10 +77,10 @@ class EventsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Models\Event  $event
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Event $event)
     {
         //
     }
