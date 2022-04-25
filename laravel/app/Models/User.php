ef57064891c60124c4c03ced408f31c349f0bbb9
Manhtq <trinhquocmanh1990@gmail.com>
2022-04-25 09:26:24 +0700

    183

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     {
         return $this->first_name . " " . $this->last_name;
     }
+
+    public function profile()
+    {
+        return $this->hasOne('App\Profile');
+    }
 }
