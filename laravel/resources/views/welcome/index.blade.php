308c02036179ae25c369567651337aa88a6ba5c4
manhtq <trinhquocmanh1990@gmail.com>
2022-04-25 15:57:01 +0700

    230

--- laravel/resources/views/welcome/index.blade.php
+++ laravel/resources/views/welcome/index.blade.php
@@ -13,6 +13,25 @@
     <p>
         <a href="{{ route('events.show', ['event' => 42]) }}" title="Laravel Hacking and Coffee">Laravel Hacking and Coffee </a>
     </p>
+    <p>
+
+        @auth
+        Welcome back, {{ Auth::user()->name }}!
+        @else
+        Hello, stranger! <a href="{{ route('login') }}">Login</a>
+        or <a href="{{ route('register') }}">Register</a>.
+        @endauth
+    </p>
+    <p>
+
+        @if (Auth::check())
+        Welcome back 2, {{ Auth::user()->name }}!
+        @else
+        Hello, stranger 2! <a href="{{ route('login') }}">Login</a>
+        or <a href="{{ route('register') }}">Register</a>.
+        @endif
+    </p>
+
 
 </body>
 
